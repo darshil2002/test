a4097c8b2a252a3ff918b1eab526135837e32482
darshil <darshil02jadav@gmail.com>
2023-10-12 00:34:39 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  data = [
+    { date: '2023-10-11', booleanValue: true },
+    { date: '2023-10-12', booleanValue: true },
+    { date: '2023-10-13', booleanValue: true },
+    { date: '2023-10-14', booleanValue: true },
+    { date: '2023-10-15', booleanValue: true },
+    { date: '2023-10-16', booleanValue: true },
+    { date: '2023-10-17', booleanValue: true },
+    { date: '2023-10-18', booleanValue: true },
+    { date: '2023-10-19', booleanValue: true },
+    { date: '2023-10-20', booleanValue: true },
+  ];
+  
+  getTooltipText(item: any): string {
+    return `Date: ${item.date}, Boolean Value: ${item.booleanValue ? 'True' : 'False'}`;
+  }
+
+  showMobileTooltip(event: MouseEvent) {
+    const target = event.currentTarget as HTMLDivElement;
+    target.click(); // Trigger click event to show the mobile tooltip
+  }
 }
